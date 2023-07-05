8d6f43151d61cd79b56f97f6f5206c12ecd101ab
SampatSuthar <45846023+SampatSuthar@users.noreply.github.com>
2023-07-05 13:48:59 +0530

    Update request.py

--- request.py
+++ request.py
@@ -3,6 +3,7 @@ import requests
 url = 'http://localhost:5000/predict_api'
 r = requests.post(url,json={'ini_level':2, 'status':1, 'time period(in days)':6, 'goal clarity':1,'experience(years)':1,'leadership':1,'completion on time':1,'budget':2,'employee talent':1,'environment':3,'adaptbility':1,'stress':1,'fear of loss':1})
 if :
+  print(yes)
 else:
-  
+  print(no)
 print(r.json())
